3e59a8a38b21545609dbe6d65432e87d7a5b6f4b
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-01-18 09:33:29 +0700

    Update build.py

--- build.py
+++ build.py
@@ -47,6 +47,7 @@ FILES_NSFW_GAMBLING = [
 FILES_BIG_DATA = [
     "filters/blocklist/RifqyShield-BlockList.txt", # Ini gabungan Hagezi + BlocklistProject
     "filters/blocklist/RifqyShield-Anti-PopUP.txt",
+    "filters/blocklist/Komdigi Blocklist.txt",
     "filters/blocklist/ultimate.txt",
 ]
 
